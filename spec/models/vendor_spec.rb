d009af3d55113322d67f8b0a331b757d223a63d8
Charles Ren <cren04@syr.edu>
2023-12-05 16:34:12 -0800

    custom errors for true/false :credit_accepted value

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -6,16 +6,30 @@ RSpec.describe Vendor, type: :model do
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:contact_name) }
     it { should validate_presence_of(:contact_phone) }
-    # it { should validate_inclusion_of(:credit_accepted).in_array([true, false]) }
-    # You are using `validate_inclusion_of` to assert that a boolean column
-    # allows boolean values and disallows non-boolean ones. Be aware that it
-    # is not possible to fully test this, as boolean columns will
-    # automatically convert non-boolean values to boolean ones. Hence, you
-    # should consider removing this test.
   end
 
   describe "relationships" do
     it { should have_many(:market_vendors)}
     it { should have_many(:markets).through(:market_vendors)}
+  end
+
+  describe 'credit_accepted validation' do
+    it 'is valid when credit_accepted is true' do
+      vendor = Vendor.new(credit_accepted: true)
+      vendor.valid?
+      expect(vendor.errors[:credit_accepted]).to be_empty
+    end
+
+    it 'is valid when credit_accepted is false' do
+      vendor = Vendor.new(credit_accepted: false)
+      vendor.valid?
+      expect(vendor.errors[:credit_accepted]).to be_empty
+    end
+
+    it 'is not valid when credit_accepted is not true or false' do
+      vendor = Vendor.new(credit_accepted: nil)
+      vendor.valid?
+      expect(vendor.errors[:credit_accepted]).to include("must be true or false")
+    end
   end
 end
